c15cd0811c9a56d8fa9c860f49c45e2273614330
Abonkwe <abonkweprincely2005@gmail.com>
2025-05-25 21:34:34 +0100

    Created authentiaction for artisans

--- Backend/config.py
+++ Backend/config.py
@@ -2,16 +2,28 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from flask_cors import CORS
+from flasgger import Swagger
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///artisanhub.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
 
+# Swaggerui cofiguration for api testing
+swagger = Swagger(app)
 
 # database configuration
-
-class Base(DeclerativeBase):
+class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
+
+# database migration
+migrate = Migrate(app=app, db=db)
+
+# authentication setup
+app.config["JWT_SECRET_KEY"] = "super-secret"
+jwt = JWTManager(app)
--- Backend/models.py
+++ Backend/models.py
@@ -1,6 +1,9 @@
 from config import db
-from sqlalchemy.orm import Mapped, mapped_column, DeclerativeBase
-from sqlalchemy import String, Integer, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
+from sqlalchemy import String, Integer, Boolean, ForeignKey
+from werkzeug.security import check_password_hash
+
+
 
 # Database Models
 
@@ -11,10 +14,19 @@ class Artisan(db.Model):
     email: Mapped[str] = mapped_column(String(40), nullable=False, unique=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
     tel: Mapped[int] = mapped_column(Integer, nullable=False)
+    password: Mapped[str] = mapped_column(String(100), nullable=False)
     verified: Mapped[bool] = mapped_column(Boolean, nullable=True)
 
-    def to_json(self):
+    # relationship between artisan and product
+    product: Mapped[int] = relationship("Product", back_populates="artisan_product", cascade="all, delete-orphan")
 
+    def check_password(self, password):
+        if check_password_hash(self.password, password):
+            return True
+        return False
+
+
+    def to_json(self):
         artisan = {}
 
         for column in self.___table___.columns():
@@ -33,17 +45,28 @@ class Customer(db.Model):
     name: Mapped[str] = mapped_column(String(50), unique=True)
     tel: Mapped[int] = mapped_column(Integer,nullable=False, unique=True)
 
-
     # converts model to json python dictionary format easily convertible to json
     def to_json(self):
-
-        artisan = {}
+        customer = {}
 
         for column in self.___table___.columns():
-            artisan[column.name] = getattr(self, column.name)
+            customer[column.name] = getattr(self, column.name)
 
-        return artisan
-
+        return customer
 
     def __str__(self):
         return self.name
+    
+
+class Product(db.Model):
+    __tablename__ = "product"
+
+    prod_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    prod_name: Mapped[str] = mapped_column(String(40), nullable=False)
+
+    # relationship between customer and product
+    artsian_id: Mapped[int] = mapped_column(Integer, ForeignKey('artisan.id'), nullable=False)
+    artisan_product: Mapped[int] = relationship("Artisan", back_populates="product")
+
+    # relationship between customer and product
+    customer_id: Mapped[int] = mapped_column(Integer, ForeignKey('customer.id'), nullable=True)
